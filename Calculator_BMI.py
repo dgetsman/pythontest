005d9d6a8a89a17c9f07ee348b0b53b1b22ef2d0
dgetsman <129435392+dgetsman@users.noreply.github.com>
2023-04-04 18:35:03 +0300

    Delete Calculator_BMI.py

--- Calculator_BMI.py
+++
@@ -1,8 +0,0 @@
-a = float(input("Введите ваш вес "))
-b = float(input("Введите ваш рост "))
-c = a / (b / 100)**2
-graf = ("15==============30")
-imt = round(c) - 15
-result = graf[:imt] + "|" + graf[imt + 1:]
-print("Ваш индекс массы тела: ", c)
-print (result)
