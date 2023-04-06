d1921b93f88a5362abcccfcf78912a0688e0511b
dgetsman <dgetsman@bk.ru>
2023-04-07 00:14:32 +0300

    practic_v1

---
+++ Practic.py
@@ -0,0 +1,13 @@
+a = int(input("Введите первое значение: ")) 
+b = int(input("Введите второе значение: ")) 
+c = int(input("Введите третье значение: ")) 
+a == 0 and print(a) or b == 0 and print(b) or c == 0 and print(c)
+a + b + c == 0 and print("Введены  все нули!") or a + b + c == 0 and print("Нет нулевых значений!!!")
+if a > (b + c):
+    print(a - b - c)
+if a < (b + c): 
+    print(b + c - a)
+if a > 50 and b > a or c > a:
+    print("Вася")
+if a > 5 and b == 7 and c == 7:
+    print("Петя")
